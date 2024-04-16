01a8d570e7190cb4434bb0358ed0fc5f6f6e638e
only-zover <martinho@alunos.utfpr.edu.br>
2024-04-16 17:59:25 -0300

    first commit

---
+++ src/test/java/com/insane/wariate/WariateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insane.wariate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WariateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
